c8caf7e6ab2e9300c06d5807ff8dd2c2ba2d21d4
Leo <leofel.katz@gmail.com>
2017-08-08 21:00:09 +0200

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+//Calculate my age - lol
+
+let myAge = 0;
+
+
+
+document.getElementById('myAge').innerHTML = getAge("1999/06/28");
+
+
+
+
+
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
